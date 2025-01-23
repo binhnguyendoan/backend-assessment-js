d27a02d8c58bf3164d849ebf68636dfe7c78a701
binhnguyendoan <binhnd782003@gmail.com>
2025-01-23 17:19:14 +0700

    Update

--- src/app.js
+++ src/app.js
@@ -1,46 +1,57 @@
 require("dotenv").config();
-
 const http = require("http");
 const { neon } = require("@neondatabase/serverless");
+const { handleRequest, json } = require("itty-router");
 
 const sql = neon(process.env.DATABASE_URL);
 
-const createTableProducts = async () => {
-    await sql`
-      CREATE TABLE IF NOT EXISTS products (
-        ProductID SERIAL PRIMARY KEY,
-        title TEXT NOT NULL,
-        tags TEXT[],
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-      )
-    `;
-    console.log("Table 'products' created or already exists.");
+  
+const createTabletest= async () => {
+    await sql `
+    CREATE TABLE IF NOT EXISTS test (
+      id SERIAL PRIMARY KEY,
+      title TEXT NOT NULL
+    )
+    `
+    ;
+    console.log("Table 'test' created or already exists.");
 };
+ const insertData = async (productData) => {
+  const { title, tags, created_at, updated_at } = productData;
+ 
+  await sql `
+  INSERT INTO products ( title, tags, created_at, updated_at)
+  VALUES ( ${title}, ${tags}, ${created_at}, ${updated_at})
+  ON CONFLICT (ProductID) DO NOTHING
+  `
+;
 
-const insertData = async (product) => {
-    const insertQuery = `
-      INSERT INTO products (ProductID, title, , tags, created_at, updated_at)
-      VALUES ($1, $2, $3, $4, $5)
-    `;
-
-    for (const variant of product.variants) {
-        await sql.query(insertQuery, [
-            product.id,
-            product.title,
-            product.tags || null,
-            product.created_at ? new Date(product.created_at) : null,
-            product.updated_at ? new Date(product.updated_at) : null,
-        ]);
-    }
+  console.log("Table 'products' insert or already exists.");
 };
 
 const requestHandler = async (req, res) => {
-    await createTableProducts();
-    const result = await sql`SELECT version()`;
-    const { version } = result[0];
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end(version);
+
+  const url = req.url;
+  const method = req.method;
+  if (url === "/api/products" && method === "GET") {
+    try {
+     
+      const result = await sql`SELECT * FROM products`; 
+      res.end(JSON.stringify({
+        success: true,
+        result: result
+      }));
+      
+    } catch (error) {
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.end("Error retrieving products.");
+    }
+  }
+    
+  const result = await "sql SELECT version()";
+  const { version } = result[0];
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end(version);
 };
 
 http.createServer(requestHandler).listen(3000, () => {
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
-import { json, Router } from 'itty-router';;
+import { json, Router } from 'itty-router';
 const router = Router();
+import { neon } from "@neondatabase/serverless";
 
 router.get("/api/products", async () => {
 	try {
 		const response = await fetch("https://02557f4d-8f03-405d-a4e7-7a6483d26a04.mock.pstmn.io/get");
-		const data = await response.json();
-		for (const product of data.products) {
-			await insertData(product);
-		}
+    
+
+	
+	  
 		return json({
 			success: true,
 			message: "Products retrieved successfully.",
